47e7b3ff987fa4bf2b436947ccc6f702ef83804c
Luciano Campos <lucianobandeiracampos@gmail.com>
2020-12-11 01:07:25 -0300

    inseridos no mapa marcadores condicionais e info-windows para os places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,22 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
-
+    @visits = Visit.all
     # the `geocoded` scope filters only places with coordinates (latitude & longitude)
     @markers = @places.geocoded.map do |place|
+      place_visits = Visit.where(place_id: place.id)
+      last_visit = place_visits.last
+      last_state = last_visit.state
+      last_focus = last_visit.focus_quantity
+      icon = 'ban.svg' if last_state == "ausente" || last_state == "visita_recusada"
+      icon = 'mosquito.svg' if last_state == "visita_realizada" && last_focus > 0
+      icon = 'check.svg' if last_focus == 0
+
       {
         lat: place.latitude,
-        lng: place.longitude
+        lng: place.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { place: place }),
+        image_url: helpers.asset_url(icon)
       }
     end
   end
